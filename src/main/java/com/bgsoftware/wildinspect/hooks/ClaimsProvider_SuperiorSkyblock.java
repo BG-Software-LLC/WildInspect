fed83d0b1ad83fa4d82f64f2948c5f570ee2e736
OmerBenGera <omerthepro126@gmail.com>
2019-10-21 00:31:36 +0300

    Fixed an issue with checking roles with superior

--- src/main/java/com/bgsoftware/wildinspect/hooks/ClaimsProvider_SuperiorSkyblock.java
+++ src/main/java/com/bgsoftware/wildinspect/hooks/ClaimsProvider_SuperiorSkyblock.java
@@ -1,8 +1,10 @@
 package com.bgsoftware.wildinspect.hooks;
 
 import com.bgsoftware.superiorskyblock.api.SuperiorSkyblockAPI;
+import com.bgsoftware.superiorskyblock.api.handlers.PlayersManager;
 import com.bgsoftware.superiorskyblock.api.island.Island;
 import com.bgsoftware.superiorskyblock.api.island.IslandPermission;
+import com.bgsoftware.superiorskyblock.api.island.PlayerRole;
 import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -12,9 +14,13 @@ import java.util.Arrays;
 public final class ClaimsProvider_SuperiorSkyblock implements ClaimsProvider{
 
     @Override
-    public boolean hasRole(Player player, String... role) {
+    public boolean hasRole(Player player, Location location, String... role) {
         SuperiorPlayer superiorPlayer = SuperiorSkyblockAPI.getPlayer(player);
-        return superiorPlayer.getIsland() == null || Arrays.asList(role).contains(superiorPlayer.getPlayerRole().toString());
+        Island island = SuperiorSkyblockAPI.getIslandAt(location);
+        PlayersManager playersManager = SuperiorSkyblockAPI.getSuperiorSkyblock().getPlayers();
+        PlayerRole playerRole = island == null || island.isMember(superiorPlayer) ? superiorPlayer.getPlayerRole() :
+                island.isCoop(superiorPlayer) ? playersManager.getCoopRole() : playersManager.getGuestRole();
+        return Arrays.asList(role).contains(playerRole.toString());
     }
 
     @Override
