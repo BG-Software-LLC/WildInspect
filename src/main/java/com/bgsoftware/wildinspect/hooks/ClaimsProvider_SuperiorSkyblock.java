748fae18716240fabb052198db318ffc13ad215c
OmerBenGera <omerthepro126@gmail.com>
2020-11-01 10:14:53 +0200

    Updated support for latest SuperiorSkyblock

--- src/main/java/com/bgsoftware/wildinspect/hooks/ClaimsProvider_SuperiorSkyblock.java
+++ src/main/java/com/bgsoftware/wildinspect/hooks/ClaimsProvider_SuperiorSkyblock.java
@@ -3,7 +3,7 @@ package com.bgsoftware.wildinspect.hooks;
 import com.bgsoftware.superiorskyblock.api.SuperiorSkyblockAPI;
 import com.bgsoftware.superiorskyblock.api.handlers.PlayersManager;
 import com.bgsoftware.superiorskyblock.api.island.Island;
-import com.bgsoftware.superiorskyblock.api.island.IslandPermission;
+import com.bgsoftware.superiorskyblock.api.island.IslandPrivilege;
 import com.bgsoftware.superiorskyblock.api.island.PlayerRole;
 import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
 import org.bukkit.Location;
@@ -12,6 +12,8 @@ import org.bukkit.entity.Player;
 import java.util.Arrays;
 
 public final class ClaimsProvider_SuperiorSkyblock implements ClaimsProvider{
+
+    private static final IslandPrivilege BUILD_PRIVILEGE = IslandPrivilege.getByName("BUILD");
 
     @Override
     public boolean hasRole(Player player, Location location, String... role) {
@@ -27,6 +29,6 @@ public final class ClaimsProvider_SuperiorSkyblock implements ClaimsProvider{
     public boolean hasRegionAccess(Player player, Location location) {
         Island island = SuperiorSkyblockAPI.getIslandAt(location);
         SuperiorPlayer superiorPlayer = SuperiorSkyblockAPI.getPlayer(player);
-        return island != null && island.hasPermission(superiorPlayer, IslandPermission.BUILD);
+        return island != null && island.hasPermission(superiorPlayer, BUILD_PRIVILEGE);
     }
 }
