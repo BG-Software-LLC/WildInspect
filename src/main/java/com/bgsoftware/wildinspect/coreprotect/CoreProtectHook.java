79e7d336adb748083b3eced27c129143bcc98a09
OmerBenGera <omerthepro126@gmail.com>
2021-10-15 18:24:25 +0300

    Potentially fixed issues with using latest builds of CoreProtect (#15)

--- src/main/java/com/bgsoftware/wildinspect/coreprotect/CoreProtectHook.java
+++ src/main/java/com/bgsoftware/wildinspect/coreprotect/CoreProtectHook.java
@@ -6,6 +6,7 @@ import net.coreprotect.database.lookup.BlockLookup;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.sql.Statement;
@@ -20,9 +21,15 @@ public final class CoreProtectHook {
     private static final ReflectMethod<String> BLOCK_LOOKUP_METHOD = new ReflectMethod<>(Lookup.class, "block_lookup", Statement.class, BlockState.class, String.class, int.class, int.class, int.class);
     private static final ReflectMethod<String> CHEST_TRANSACTION_LOOKUP_METHOD = new ReflectMethod<>(Lookup.class, "chest_transactions", Statement.class, Location.class, String.class, int.class, int.class, boolean.class);
 
+    private static final ReflectMethod<String> INTERACTION_LOOKUP_V20_METHOD = new ReflectMethod<>("net.coreprotect.database.lookup.InteractionLookup", "performLookup", String.class, Statement.class, Block.class, CommandSender.class, int.class, int.class, int.class);
+    private static final ReflectMethod<String> BLOCK_LOOKUP_V20_METHOD = new ReflectMethod<>("net.coreprotect.database.lookup.BlockLookup", "performLookup", String.class, Statement.class, BlockState.class, CommandSender.class, int.class, int.class, int.class);
+    private static final ReflectMethod<String> CHEST_TRANSACTION_LOOKUP_V20_METHOD = new ReflectMethod<>("net.coreprotect.database.lookup.ChestTransactionLookup", "performLookup", String.class, Statement.class, Location.class, CommandSender.class, int.class, int.class, boolean.class);
+
     public static String[] performInteractLookup(Statement statement, Player player, Block block, int page) {
         if (INTERACTION_LOOKUP_METHOD.isValid()) {
             return parseResult(INTERACTION_LOOKUP_METHOD.invoke(null, statement, block, player.getName(), 0, page, 7));
+        } else if (INTERACTION_LOOKUP_V20_METHOD.isValid()) {
+            return parseResult(INTERACTION_LOOKUP_V20_METHOD.invoke(null, statement, block, player, 0, page, 7));
         } else {
             return parseResult(Lookup.interactionLookup(null, statement, block, player, 0, page, 7));
         }
@@ -31,6 +38,8 @@ public final class CoreProtectHook {
     public static String[] performBlockLookup(Statement statement, Player player, BlockState blockState, int page) {
         if (BLOCK_LOOKUP_METHOD.isValid()) {
             return parseResult(BLOCK_LOOKUP_METHOD.invoke(null, statement, blockState, player.getName(), 0, page, 7));
+        } else if (BLOCK_LOOKUP_V20_METHOD.isValid()) {
+            return parseResult(BLOCK_LOOKUP_V20_METHOD.invoke(null, statement, blockState, player, 0, page, 7));
         } else {
             return parseResult(BlockLookup.results(null, statement, blockState, player, 0, page, 7));
         }
@@ -39,6 +48,8 @@ public final class CoreProtectHook {
     public static String[] performChestLookup(Statement statement, Player player, Block block, int page) {
         if (CHEST_TRANSACTION_LOOKUP_METHOD.isValid()) {
             return parseResult(CHEST_TRANSACTION_LOOKUP_METHOD.invoke(null, statement, block.getLocation(), player.getName(), page, 7, false));
+        } else if (CHEST_TRANSACTION_LOOKUP_V20_METHOD.isValid()) {
+            return parseResult(CHEST_TRANSACTION_LOOKUP_V20_METHOD.invoke(null, statement, block.getLocation(), player, page, 7, false));
         } else {
             return parseResult(Lookup.chestTransactions(null, statement, block.getLocation(), player, page, 7, false));
         }
