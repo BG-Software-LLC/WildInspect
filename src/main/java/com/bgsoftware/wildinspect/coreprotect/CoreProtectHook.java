71ed4de4597c9de100bc674d8f42dc7a0718741c
OmerBenGera <omerthepro126@gmail.com>
2021-10-15 18:15:51 +0300

    Removed usage of raw reflection in favor of ReflectionUtils library

--- src/main/java/com/bgsoftware/wildinspect/coreprotect/CoreProtectHook.java
+++ src/main/java/com/bgsoftware/wildinspect/coreprotect/CoreProtectHook.java
@@ -1,67 +1,53 @@
 package com.bgsoftware.wildinspect.coreprotect;
 
+import com.bgsoftware.common.reflection.ReflectMethod;
 import net.coreprotect.database.Lookup;
-
 import net.coreprotect.database.lookup.BlockLookup;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 
-import java.lang.reflect.Method;
 import java.sql.Statement;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@SuppressWarnings("JavaReflectionMemberAccess")
 public final class CoreProtectHook {
 
     private static final Pattern COMPONENT_PATTERN = Pattern.compile("<COMPONENT>(.*)\\|(.*)\\|(.*)</COMPONENT>");
 
-    private static Method interactionLookupMethod = null, blockLookupMethod = null, chestTransactionsLookup = null;
-
-    static {
-        try {
-            interactionLookupMethod = Lookup.class.getMethod("interaction_lookup", Statement.class, Block.class, String.class, int.class, int.class, int.class);
-            blockLookupMethod = Lookup.class.getMethod("block_lookup", Statement.class, BlockState.class, String.class, int.class, int.class, int.class);
-            chestTransactionsLookup = Lookup.class.getMethod("chest_transactions", Statement.class, Location.class, String.class, int.class, int.class, boolean.class);
-        }catch(Exception ignored){
-//            try{
-//                interactionLookupMethod = Lookup.class.getMethod("interactionLookup", String.class, Statement.class, Block.class, CommandSender.class, int.class, int.class, int.class);
-//                blockLookupMethod = BlockLookup.class.getMethod("results", String.class, Statement.class, BlockState.class, CommandSender.class, int.class, int.class, int.class);
-//                chestTransactionsLookup = Lookup.class.getMethod("chestTransactions", String.class, Statement.class, Location.class, CommandSender.class, int.class, int.class, boolean.class);
-//            }catch (Exception ignored2){}
-        }
-    }
+    private static final ReflectMethod<String> INTERACTION_LOOKUP_METHOD = new ReflectMethod<>(Lookup.class, "interaction_lookup", Statement.class, Block.class, String.class, int.class, int.class, int.class);
+    private static final ReflectMethod<String> BLOCK_LOOKUP_METHOD = new ReflectMethod<>(Lookup.class, "block_lookup", Statement.class, BlockState.class, String.class, int.class, int.class, int.class);
+    private static final ReflectMethod<String> CHEST_TRANSACTION_LOOKUP_METHOD = new ReflectMethod<>(Lookup.class, "chest_transactions", Statement.class, Location.class, String.class, int.class, int.class, boolean.class);
 
     public static String[] performInteractLookup(Statement statement, Player player, Block block, int page) {
-        try{
-            return parseResult((String) interactionLookupMethod.invoke(null, statement, block, player.getName(), 0, page, 7));
-        }catch (Throwable ex){
+        if (INTERACTION_LOOKUP_METHOD.isValid()) {
+            return parseResult(INTERACTION_LOOKUP_METHOD.invoke(null, statement, block, player.getName(), 0, page, 7));
+        } else {
             return parseResult(Lookup.interactionLookup(null, statement, block, player, 0, page, 7));
         }
     }
 
     public static String[] performBlockLookup(Statement statement, Player player, BlockState blockState, int page) {
-        try{
-            return parseResult((String) blockLookupMethod.invoke(null, statement, blockState, player.getName(), 0, page, 7));
-        }catch (Throwable ex){
+        if (BLOCK_LOOKUP_METHOD.isValid()) {
+            return parseResult(BLOCK_LOOKUP_METHOD.invoke(null, statement, blockState, player.getName(), 0, page, 7));
+        } else {
             return parseResult(BlockLookup.results(null, statement, blockState, player, 0, page, 7));
         }
     }
 
     public static String[] performChestLookup(Statement statement, Player player, Block block, int page) {
-        try{
-            return parseResult((String) chestTransactionsLookup.invoke(null, statement, block.getLocation(), player.getName(), page, 7, false));
-        }catch (Throwable ex){
+        if (CHEST_TRANSACTION_LOOKUP_METHOD.isValid()) {
+            return parseResult(CHEST_TRANSACTION_LOOKUP_METHOD.invoke(null, statement, block.getLocation(), player.getName(), page, 7, false));
+        } else {
             return parseResult(Lookup.chestTransactions(null, statement, block.getLocation(), player, page, 7, false));
         }
     }
 
-    private static String[] parseResult(String result){
+    private static String[] parseResult(String result) {
         Matcher matcher = COMPONENT_PATTERN.matcher(result);
 
-        if(matcher.find())
+        if (matcher.find())
             result = matcher.replaceAll("$3");
 
         return result.split("\n");
